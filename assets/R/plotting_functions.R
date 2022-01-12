b8901969f2605f4d0265f2815440a91317d03855
@daaronr <daaronr@gmail.com>
2022-01-12 10:30:04 -0500

    add some functions to consider including (or deleting)

--- assets/R/plotting_functions.R
+++ assets/R/plotting_functions.R
@@ -152,3 +152,106 @@ grouped_forest_plot <- function(coefs, groups, ordered = TRUE,
   return(forest)
 
 }
+
+
+### Additional plotting functions in functions.R, not used in EAS work
+
+
+
+# VISUALISATION functions: ####
+
+#TODO -- add ggrepel to these? geom_label_repel() and geom_text_repel
+
+plot_histogram <- function(df, feature) {
+  chart_title <- substitute(paste("Histogram of ", feature,
+    sep = ""))
+  plt <- ggplot(df, aes(x = eval(parse(text = feature)))) +
+    geom_histogram(aes(y = ..density..), alpha = 0.7, fill = "#33AADE",
+      color = "black") + geom_density(alpha = 0.3, fill = "red") +
+    geom_vline(aes(xintercept = mean(eval(parse(text = feature)))),
+      color = "black", linetype = "dashed", size = 1) +
+    labs(x = feature, y = "Density")
+  print(plt)
+}
+
+# ... Multiple histogram: ####
+
+plot_multi_histogram <- function(df, feature, label_column) {
+  chart_title <- substitute(paste("Histograms of ", feature,
+    " by ", label_column, sep = ""))
+  plt <- ggplot(df, aes(x = eval(parse(text = feature)), fill = eval(parse(text = label_column)))) +
+    geom_histogram(alpha = 0.3, position = "identity", aes(y = ..density..),
+      color = "black") + geom_density(alpha = 0.3) + geom_vline(aes(xintercept = mean(eval(parse(text = feature)))),
+    color = "black", linetype = "dashed", size = 1) + labs(title = chart_title,
+    subtitle = "dashed line = mean", x = feature, y = "Density")
+  plt + guides(fill = guide_legend(title = label_column))
+}
+
+
+dotplot_func <- function(df = ADSX, yvar = donation, xvar = Stage,
+  treatvar = Treatment, title = "") {
+  yvar <- enquo(yvar)
+  xvar <- enquo(xvar)
+  treatvar <- enquo(treatvar)
+  df %>% ungroup() %>% # mutate(xvar = as.factor(!!xvar)) %>%
+  dplyr::group_by(!!xvar, !!treatvar) %>% # drop_na(!!yvar, !!treatvar) %>%
+  dplyr::select(!!yvar, !!treatvar, !!xvar) %>% dplyr::summarise(meanyvar = mean(!!yvar,
+    na.rm = TRUE)) %>% ggplot(aes(y = meanyvar, x = !!xvar,
+    color = !!treatvar, group = !!treatvar, shape = !!treatvar)) +
+    geom_point(size = 6) + geom_line() + expand_limits(y = 0) +
+    scale_x_continuous(breaks = c(1, 2)) + scale_y_continuous(yvar) +
+    theme(panel.grid.major.y = element_line(color = "white",
+      size = 0.3)) + theme(panel.grid.minor.y = element_line(color = "white",
+    size = 0.1)) + theme(panel.grid.major.x = element_blank()) +
+    theme(panel.grid.minor.x = element_blank()) + theme(axis.ticks.x = element_blank()) +
+    theme(axis.title = element_text(size = 14), axis.text = element_text(size = 12)) +
+    theme(legend.text = element_text(size = 12), legend.title = element_text(size = 15,
+      face = "bold")) + ggtitle(title)
+}
+
+geom_mean <- function() {
+  list(stat_summary(fun.y = "mean", geom = "point", fill = "red"),
+    stat_summary(fun.data = "mean_cl_normal", geom = "errorbar",
+      width = 0.4))
+}
+
+boxplot_func <- function(df = ADSX, yvar = donation, treatvar = Treatment, facetfunc = 1,
+  comparisons = list(c("No ask-Domestic", "Domestic-Domestic"))) {
+  yv <- enquo(yvar)
+  tv <- enquo(treatvar)
+  ungroup(df) %>%
+      group_by({{treatvar}}) %>%
+      drop_na({{yvar}}, {{treatvar}}) %>%
+    ggplot(aes({{treatvar}}, {{yvar}})) +
+    geom_boxplot() +
+    facet_grid({{facetfunc}}) +
+    theme(axis.title = element_text(size = 14),
+    axis.text = element_text(size = 14)) +
+    theme(axis.text.x = element_text(size = 12)) +
+    labs(title = tv, y = yv, caption = "p-values of Wilcox-(below) and  t-test (above brackets)") +
+    geom_signif(comparisons = comparisons, step_increase = c(0.4),
+      vjust = 1.7, margin_top = 0.5, textsize = 5) +
+    geom_signif(comparisons = comparisons,
+    step_increase = c(0.4), vjust = 0, margin_top = 0.5,
+    textsize = 5, test = "t.test") +
+     stat_summary(
+    fun.y = mean, geom = "point", shape = 18,
+      size = 3, color = "red")
+}
+
+# https://stackoverflow.com/questions/13407236/remove-a-layer-from-a-ggplot2-chart
+# Delete layers from ggplot
+remove_geom <- function(ggplot2_object, geom_type) {
+  # Delete layers that match the requested type.
+  layers <- lapply(ggplot2_object$layers, function(x) {
+    if (class(x$geom)[1] == geom_type) {
+      NULL
+    } else {
+      x
+    }
+  })
+  # Delete the unwanted layers.
+  layers <- layers[!sapply(layers, is.null)]
+  ggplot2_object$layers <- layers
+  ggplot2_object
+}
