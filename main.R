1b182e7ea5dd80ed51642a36eff5d74fad7b7cb8
willemsleegers <w.sleegers@me.com>
2024-09-02 18:56:41 +0200

    Restructuring and added more styling to bar plots

--- main.R
+++
@@ -1,9 +0,0 @@
-# One R file to do everything
-
-# Setup -------------------------------------------------------------------
-
-# Bring in data, packages, etc, whatever else you want to do here
-
-# Render book -------------------------------------------------------------
-
-quarto::quarto_render()
